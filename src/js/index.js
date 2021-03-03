f85645dc671a308790fe3a74e55d1a06496b665c
OnixFang <twily.dev@outlook.com>
2021-03-02 23:16:21 -0400

    Add salutation Easter egg

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,16 @@
-import 'materialize-css';
+const salutations = ['Hello!', 'Hi!', 'Hey!', "What's up!?"];
+let helloTimeOut;
 
-const babeltest = () => {
-  console.log('Scripts are working.');
-};
+const heroPic = document.getElementsByClassName('hero-picture')[0];
 
-babeltest();
+heroPic.addEventListener('click', () => {
+  const helloElement = document.getElementsByClassName('hello')[0];
+  helloElement.innerHTML = salutations[Math.floor(Math.random() * 4)];
+  helloElement.classList.add('show');
+
+  clearTimeout(helloTimeOut);
+
+  helloTimeOut = setTimeout(() => {
+    helloElement.classList.remove('show');
+  }, 2000);
+});
