0ff51425053839500da191ecc47629e8c46908a1
OnixFang <twily.dev@outlook.com>
2021-03-16 21:18:20 -0400

    Setup navbar section detection

--- src/js/index.js
+++ src/js/index.js
@@ -30,17 +30,46 @@ const observerFunction = (entries) => {
     const { animation } = element.dataset;
 
     if (entry.isIntersecting) {
-      element.style.animation = `${animation} 500ms ease ${getRandom(
-        200
-      )}ms forwards`;
+      element.style.animation = `${animation} 500ms ease ${getRandom(200)}ms forwards`;
     } else {
       element.style.animation = 'none';
     }
   });
 };
 
-const observer = new IntersectionObserver(observerFunction);
+const animationObserver = new IntersectionObserver(observerFunction);
 
 elements.forEach((element) => {
-  observer.observe(element);
+  animationObserver.observe(element);
+});
+
+// Intersection Observer for navbar
+const sections = document.querySelectorAll('section');
+const navlinks = Array.from(document.querySelectorAll('.navlink'));
+
+const sectionObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      const section = entry.target;
+      const navlink = navlinks.find((link) => link.href.endsWith(`#${section.id}`));
+
+      if (entry.isIntersecting) {
+        navlink.classList.add('active');
+        let prevIndex = navlinks.indexOf(navlink) - 1;
+        let nextIndex = navlinks.indexOf(navlink) + 1;
+
+        navlinks[nextIndex].classList.remove('active');
+        if (prevIndex >= 0) {
+          navlinks[prevIndex].classList.remove('active');
+        }
+      } else {
+        navlink.classList.remove('active');
+      }
+    });
+  },
+  { threshold: 0.5 }
+);
+
+sections.forEach((section) => {
+  sectionObserver.observe(section);
 });
